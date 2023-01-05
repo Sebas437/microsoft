313d93c8f0a8a3c26b112d03b8622a2cb640b8b5
sebas437 <jsebas4373299@gmail.com>
2023-01-04 20:53:50 -0500

    microsoft

---
+++ src/Config.js
@@ -0,0 +1,8 @@
+export const config ={
+    appId:"47448771-18ac-4b78-93f8-f10bad6d913b",
+    redirectUri:"http://localhost:3000/",
+    scopes:[
+        "user.read"
+    ],
+    authority:"https://login.microsoftonline.com/common"
+};
